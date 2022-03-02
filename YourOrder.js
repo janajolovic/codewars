0ff937991805a05e4630081c9cf7bd7b4dffd56e
Jana Jolovic <janajolovic04@gmail.com>
2022-03-02 21:39:17 +0100

    your order please

--- YourOrder.js
+++ YourOrder.js
@@ -1,15 +1,13 @@
-function order(words) {
-    const findNumber = words.match(/[1-9]/g);
-    const arr = words.split(" ");
-    let res = [];
-    if (words.length === 0) return "";
-    
-    arr.map((el, i) => {
-      const index = findNumber.indexOf(i+1);
-      res.push(arr[index]);
-    });
-    return res.join(' ');
+function order(words){
+    if (words.length === 0) return words
+    arr = words.split(' ');
+    isNum = words.match(/\d/g);
+    res = [];
+    for (i=1; i<=isNum.length; i++){
+      ind = isNum.indexOf(i.toString())
+      res.push(arr[ind])
+    }
+    return res.join(' ')
 }
-
 
 console.log(order("is2 Thi1s T4est 3a"))
