bf9914975321c49578dc9a03be2f0da1f6b681f2
Jana Jolovic <janajolovic04@gmail.com>
2022-07-07 23:27:35 +0200

    initial

--- CensoredStrings.js
+++ CensoredStrings.js
@@ -1,14 +1,14 @@
-function uncensor(infected, discovered) {
-    counter = 0;
-    infected = infected.split("")
-    for (i=0; i<infected.length; i++) {
-      if (infected[i] === "*") {
-        infected[i] = discovered[counter];
-        counter += 1;
-      }
-    }
-    return infected.join("")
-}
+// function uncensor(infected, discovered) {
+//     counter = 0;
+//     infected = infected.split("")
+//     for (i=0; i<infected.length; i++) {
+//       if (infected[i] === "*") {
+//         infected[i] = discovered[counter];
+//         counter += 1;
+//       }
+//     }
+//     return infected.join("")
+// }
 
 
-console.log(uncensor('A**Z*N*', 'MAIG'))
+// console.log(uncensor('A**Z*N*', 'MAIG'))
